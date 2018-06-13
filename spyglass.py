1d8c79722386502bfc3c745e6468deddb8f2e090
Steve Anderson <stevenix@protonmail.com>
2018-06-12 21:05:08 -0500

    added blacklist functionality

--- spyglass.py
+++ spyglass.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import IPAddress
 from ipwhois import IPWhois
+import dns.resolver
 import pprint
 
 
@@ -16,8 +17,40 @@ class IPAddressForm(FlaskForm):
 def retrieve_asn(ipaddress):
     obj = IPWhois(ipaddress)
     results = obj.lookup_rdap()
-    pprint.pprint(results)
+    #pprint.pprint(results)
     return results
+
+def get_blacklists(ipaddress):
+    bl_list = ['zen.spamhaus.org',
+               'spam.abuse.ch',
+               'cbl.abuseat.org',
+               'virbl.dnsbl.bit.nl',
+               'dnsbl.inps.de',
+               'ix.dnsbl.manitu.net',
+               'dnsbl.sorbs.net',
+               'bl.spamcop.net',
+               'xbl.spamhaus.org',
+               'pbl.spamhaus.org',
+               'dnsbl-1.uceprotect.net',
+               'dnsbl-2.uceprotect.net',
+               'dnsbl-3.uceprotect.net',
+               'db.wpbl.info']
+    bl_dict = {}
+    for bl in bl_list:
+        try:
+            my_resolver = dns.resolver.Resolver()
+            query = '.'.join(reversed(str(ipaddress).split('.'))) + '.' + bl
+            answers = my_resolver.query(query, 'A')
+            answer_txt = my_resolver.query(query, 'TXT')
+            bl_dict[bl] = True
+
+        except dns.resolver.NXDOMAIN:
+            bl_dict[bl] = False
+        except dns.resolver.NoAnswer:
+            bl_dict[bl] = False
+        except dns.resolver.NoNameservers as error:
+            print(error)
+    return bl_dict
 
 @app.route('/')
 def index():
@@ -48,6 +81,10 @@ def analyze():
         network_handle = asn_data['network']['handle']
         network_name = asn_data['network']['name']
         asn_country_code = asn_data['asn_country_code']
+        blacklists = get_blacklists(ipaddress)
+        print(blacklists)
+        #print(blacklists)
+        
     else:
         flash('Not a valid IPv4 Address')
 
@@ -59,4 +96,5 @@ def analyze():
             network_name=network_name,
             asn_description=asn_description,
             asn_country_code=asn_country_code,
+            blacklists=blacklists,
             form=form)
