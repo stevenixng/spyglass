3183cf29f90341c6da4e9893d63d0215942291c9
Steve Anderson <stevenix@protonmail.com>
2018-06-13 15:09:36 -0500

    added about section

--- spyglass.py
+++ spyglass.py
@@ -71,6 +71,10 @@ def index():
 
     return render_template('index.html', form=form)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
     form = IPAddressForm(request.form)
